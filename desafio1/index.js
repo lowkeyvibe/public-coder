19a8e3c99aad76eeb75b7ba375af34bb53910af3
-lowkey <carbonelauti@gmail.com>
2024-02-10 09:20:15 -0300

    Desafio2

--- desafio1/index.js
+++ desafio1/index.js
@@ -1,65 +1,136 @@
+const fs = require("fs").promises
+
 class ProductManager {
-    constructor() {
+    constructor(path) {
         // Se crea el array!
         this.products = [],
-            this.productId = 1
+        this.productId = 1
+        this.path = path;
     }
 
     // Metodo 1
-    addProducts(title, description, price, thumbnail, code, stock) {
+    async addProduct(title, description, price, thumbnail, code, stock) {
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log("Missing fields!");
-            return
+        try {
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                console.log("Missing fields!");
+                return
 
-        } else if (this.products.some(number => number.code === code)) {
-            console.warn("Ya existe el producto!");
-            return
+            } else if (this.products.some(number => number.code === code)) {
+                console.warn("Already exist!!");
+                return
 
-        } else {
-            const id = this.productId++
-            this.products.push({
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock,
-                id
-            })
+            } else {
+                const id = this.productId++
+                const newProduct = {
+                    title,
+                    description,
+                    price,
+                    thumbnail,
+                    code,
+                    stock,
+                    id
+                }
+
+                this.products.push(newProduct)
+                await this.writeProducts(this.products)
+
+            }
+
+        } catch (error) {
+            console.warn("Error ADDING PRODUCT!", error);
         }
     }
 
     // Metodo 2
-    getProducts() {
+    async getProducts() {
+        await this.readProducts()
         console.log(this.products);
     }
 
     // Metodo 3
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id)
-        if (product) {
-            console.log(product);
-        } else {
-            console.warn("Not found!");
+    async getProductById(id) {
+        try {
+            await this.readProducts()
+            const product = this.products.find(product => product.id === id)
+
+            if (product) {
+                console.log(product);
+            } else {
+                console.warn("Product NOT FOUND!");
+            }
+
+        } catch (error) {
+            console.warn("ERROR!");
+        }
+
+    }
+
+    // Metodo 4
+    async readProducts() {
+        try {
+            const response = await fs.readFile(this.path, "utf-8");
+            const products = JSON.parse(response);
+            this.products = products;
+
+        } catch (error) {
+            this.products = []
         }
     }
+
+    // Metodo 5
+    async writeProducts(newProducts) {
+        try {
+            await fs.writeFile(this.path, JSON.stringify(newProducts, null, 2))
+        } catch (error) {
+            console.error("Error WRITING PRODUCT!", error)
+        }
+    }
+
+    // Metodo 6
+    async updateProduct(id, updatedProduct) {
+        try {
+            await this.readProducts()
+            const productIndex = this.products.findIndex(product => product.id === id)
+
+            if (productIndex === -1) {
+                console.error("Not found TO UPDATE!")
+                return
+            }
+
+            this.products[productIndex] = {
+                ...this.products[productIndex],
+                ...updatedProduct,
+                id: this.products[productIndex].id
+            };
+
+            await this.writeProducts(this.products);
+
+        } catch (error) {
+            console.error("Error updating product", error)
+        }
+    }
+
+    // Metodo 7
+    async deleteProduct(id) {
+        await this.readProducts()
+        const arrayFiltrado = this.products.filter(i => i.id != id)
+        await this.writeProducts(arrayFiltrado)
+      }
+
 }
 
-const productsArray = new ProductManager()
+const productList = new ProductManager("./products.json")
 
-productsArray.addProducts("Silla", "Mueble", 1000, "Ruta-img", "1020", 10)
-productsArray.addProducts("Mesa", "Mueble", 1000, "Ruta-img", "3040", 10)
-productsArray.addProducts("Armario", "Mueble", 1000, "Ruta-img", "5060", 10)
-productsArray.addProducts("Cama", "Mueble", 1000, "Ruta-img", "7080", 10)
+// Acá inicia el testing!
+productList.addProduct("Silla", "Mueble", 1000, "Ruta-img", "1020", 10) // Agrega productos al .JSON
+productList.addProduct("Silla", "Mueble", 1000, "Ruta-img", "1030", 10) // Agrega productos al .JSON
+productList.getProductById(2) // Retorna la segunda silla
+productList.getProducts() // Muestra el listado de productos
 
-productsArray.getProductById(1); // Retorna Silla 
-productsArray.getProducts(); // Retorna todos los productos
-
-
-
-
-
-
-
-
+productList.deleteProduct(2) // Borra del .JSON el item con ID = 2
+productList.updateProduct(1, { // Hace un Update del item con ID = 1
+    title: "Prueba 540, estoy sufriendo",
+    price: 3000,
+    stock: 500
+})
